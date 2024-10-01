589759c80cc5c8fba9159bf01165c3eb97651c88
Juan Senen <juansenen@hotmail.com>
2024-10-01 14:59:23 +0200

    Initial Commit

---
+++ TheBlueAppTests/TheBlueAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheBlueAppTests.swift
+//  TheBlueAppTests
+//
+//  Created by JSenen on 1/10/24.
+//
+
+import Testing
+@testable import TheBlueApp
+
+struct TheBlueAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
